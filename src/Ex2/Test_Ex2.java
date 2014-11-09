85787ed910b418d9646c7cc66629d5dc25a7a77d
BenetPerarnau <benetperarnau@hotmail.com>
2014-11-09 12:38:41 +0100

    retoques

--- src/Ex2/Test_Ex2.java
+++ src/Ex2/Test_Ex2.java
@@ -7,15 +7,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.font.TextAttribute;
 import java.util.Map;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JToggleButton;
-import javax.swing.ScrollPaneConstants;
 
 
 public class Test_Ex2 extends JFrame implements ActionListener {
@@ -32,9 +27,7 @@ public class Test_Ex2 extends JFrame implements ActionListener {
 	
 	public Test_Ex2() {
 		
-		this.setTitle("TestEditLayout");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+	
 		//container
 		JPanel contenedor=(JPanel)this.getContentPane();
 		contenedor.setLayout(null);
@@ -44,12 +37,9 @@ public class Test_Ex2 extends JFrame implements ActionListener {
 		//textArea
 		textArea=new JTextArea("");
 		textArea.setBounds(5+insets.left, 5+insets.top, 250, 200);
-		Font normal = new Font("Arial", Font.PLAIN, 14);
 		textArea.setFont(normal);
-
-		
 		contenedor.add(textArea);
-		
+	
 		//button Subrayar
 		btnSub=new JToggleButton("SUBRAYAR");
 		btnSub.setBounds(textArea.getWidth()+5, 10+insets.top, 90, 25);
@@ -65,8 +55,12 @@ public class Test_Ex2 extends JFrame implements ActionListener {
 		btnCur.setBounds(textArea.getWidth()+5,110,90,25);
 		btnCur.addActionListener(this);
 		contenedor.add(btnCur);
+	
 		
-		//this.setResizable(false);
+		//pantalla principal
+		this.setTitle("Editor de Texto");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
 		this.setSize(new Dimension(350,230));
 		this.setVisible(true);
 	}
@@ -75,21 +69,22 @@ public class Test_Ex2 extends JFrame implements ActionListener {
 	
 	public static void main(String[] args) {
 		
-		Test_Ex2 a=new Test_Ex2();
+		Test_Ex2 test=new Test_Ex2();
 	}
 
 
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		if(attributes!=null){
+			attributes.clear();
+		}
 		Font font = this.textArea.getFont();
 		attributes = font.getAttributes();
 		
-		
 		if(this.textArea.getText().length()!=0){
 			
-			if(e.getSource()==btnSub){//Boton subrayado
+			if(e.getSource()==btnSub){//Pulsado Boton subrayado
 				
 				if(btnSub.isSelected()){//Boton subrayado => seleccionado
 					
@@ -109,8 +104,10 @@ public class Test_Ex2 extends JFrame implements ActionListener {
 					}else if(btnCur.isSelected()){//+cursiva
 						
 						this.textArea.setFont(cursiva.deriveFont(attributes));
+						System.out.println("Subrayado + Cursiva");
 					}else{//solo subrayado
 						this.textArea.setFont(normal.deriveFont(attributes));
+						System.out.println("Subrayado");
 					}
 				}else{//Boton subrayado => no seleccionado
 					
@@ -119,41 +116,46 @@ public class Test_Ex2 extends JFrame implements ActionListener {
 					if(btnNeg.isSelected() && btnCur.isSelected() ){//+ negrita y cursiva
 						
 						this.textArea.setFont(cursivaNegrita.deriveFont(attributes));
+						System.out.println(" NO Subrayado + Negrita + Cursiva");
 					
 					}else if(btnNeg.isSelected()){ //+ negrita	
 						
 						this.textArea.setFont(negrita.deriveFont(attributes));
+						System.out.println("NO Subrayado + Negrita ");
 				
 					}else if(btnCur.isSelected()){//+cursiva
 						
 						this.textArea.setFont(cursiva.deriveFont(attributes));
+						System.out.println("NO Subrayado + Cursiva");
 					}else{
 						this.textArea.setFont(normal.deriveFont(attributes));
+						System.out.println("NO Subrayado");
 					}
 				}
 				
-			}else if(e.getSource()==btnNeg){//negrita 
-				
+			}else if(e.getSource()==btnNeg){//Pulsado boton Negrita 
 				if(btnNeg.isSelected()){//Boton negrita => Seleccionado
 					
 					if(btnSub.isSelected() && btnCur.isSelected() ){//+ Subrayado y cursiva
-
+						
+						attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
 						this.textArea.setFont(cursivaNegrita.deriveFont(attributes));
-						System.out.println("Negita + Subrayado + Cursiva");
+						System.out.println("Negrita + Subrayado + Cursiva");
 					
 					}else if(btnSub.isSelected()){ //+ Subrayado	
 						
 						attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
-						this.textArea.setFont(negrita.deriveFont(attributes));
-						System.out.println("Negita + Subrayado");
+						this.textArea.setFont(negrita.deriveFont(attributes));	
+						System.out.println("Negrita + Subrayado");
 						
 					}else if(btnCur.isSelected()){//+cursiva
 						
 						this.textArea.setFont(cursivaNegrita);
+						System.out.println("Negrita + Cursiva");
 						
 					}else{//solo negrita
 						this.textArea.setFont(negrita);
-						System.out.println("solo negrita");
+						System.out.println("Negrita");
 					}
 
 				}else{//Boton negrita => NO Seleccionado
@@ -161,69 +163,84 @@ public class Test_Ex2 extends JFrame implements ActionListener {
 					
 					if(btnSub.isSelected() && btnCur.isSelected() ){//+ Subrayado y cursiva
 						
-						//attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+						attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
 						this.textArea.setFont(cursiva.deriveFont(attributes));
+						System.out.println("NO Negrita + Subrayado + Cursiva");
 					
 					}else if(btnSub.isSelected()){ //+ Subrayado	
 						
-						//attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+						attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
 						this.textArea.setFont(normal.deriveFont(attributes));
+						System.out.println("NO Negrita + Subrayado");
 				
 					}else if(btnCur.isSelected()){//+cursiva
 						
 						this.textArea.setFont(cursiva);
+						System.out.println("NO Negrita +Cursiva");
 					}else{
 						this.textArea.setFont(normal);
+						System.out.println("NO Negrita");
 					}
 					
 				}		
-			}else if(e.getSource()==btnCur){//Boton Cursiva
+			}else if(e.getSource()==btnCur){//Pulsado boton Cursiva
 				
 				if(btnCur.isSelected()){//Boton Cursiva => Seleccionado
 						
 						if(btnSub.isSelected() && btnNeg.isSelected() ){//+ Subrayado y Negrita
 							
-							//attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
-
+						attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
 							this.textArea.setFont(cursivaNegrita.deriveFont(attributes));
+							System.out.println("Cursiva + Subrayado + Negrita");
 						
 						}else if(btnSub.isSelected()){ //+ Subrayado	
 							
-							//attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+							attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
 							this.textArea.setFont(cursiva.deriveFont(attributes));
+							System.out.println("Cursiva + Subrayado");
 					
 						}else if(btnNeg.isSelected()){//+Negrita
 							
 							this.textArea.setFont(cursivaNegrita);
+							System.out.println("Cursiva + Negrita");
 						}else{//solo Cursiva
 							
 							this.textArea.setFont(cursiva);
+							System.out.println("Cursiva");
 						}
 		
 				}else{//Boton Cursiva => NO Seleccionado
 				
 					if(btnSub.isSelected() && btnNeg.isSelected() ){//+ Subrayado y Negrita
 						
-						//attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
-
+						attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
 						this.textArea.setFont(negrita.deriveFont(attributes));
+						System.out.println("NO Cursiva + Subrayado + Negrita");
 					
 					}else if(btnSub.isSelected()){ //+ Subrayado	
 						
-						//attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+						attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
 						this.textArea.setFont(normal.deriveFont(attributes));
+						System.out.println("NO Cursiva + Subrayado");
 				
 					}else if(btnNeg.isSelected()){//+Negrita
 						
 						this.textArea.setFont(negrita);
+						System.out.println("NO Cursiva +Negrita");
 					}else{//solo Cursiva
 						
 						this.textArea.setFont(normal);
+						System.out.println("NO Cursiva");
 					}	
 				}
 		}else{
-			//futuras opciones		
+			//futuros botones con opciones		
 		}
+	}else{
+		//text area vacio
+		btnSub.setSelected(false);
+		btnNeg.setSelected(false);
+		btnCur.setSelected(false);
 	}
 }
 }
